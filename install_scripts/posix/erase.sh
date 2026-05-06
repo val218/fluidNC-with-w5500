9f53d780b1c1c2a176c49a3b1c6c1a6ca7370b24
dpCreator.eu <105395725+val218@users.noreply.github.com>
2026-05-06 17:34:40 +0300

    Delete install_scripts directory

--- install_scripts/posix/erase.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if ! . ./tools.sh; then exit 1; fi
-
-export chip="esp32"
-
-esptool_erase
-
-deactivate
--- install_scripts/posix/erase_s3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if ! . ./tools.sh; then exit 1; fi
-
-export chip="esp32s3"
-
-esptool_erase
-
-deactivate
